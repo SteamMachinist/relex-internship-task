dfc01b6e878cfdb45f9e67ab20121423ba280c2d
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-03-08 15:46:58 +0300

    init

---
+++ src/test/java/steammachinist/relexinternshiptask/RelexInternshipTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package steammachinist.relexinternshiptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelexInternshipTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
