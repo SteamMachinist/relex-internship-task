2d25f0ac5c55b4a36a2269a60d22711d3f72e24d
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-03-18 18:57:39 +0300

    implemented Product controller with add, update and get-all

---
+++ src/main/java/steammachinist/relexinternshiptask/controller/ProductController.java
@@ -0,0 +1,38 @@
+package steammachinist.relexinternshiptask.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import steammachinist.relexinternshiptask.dto.ProductDto;
+import steammachinist.relexinternshiptask.dto.request.AddProductRequest;
+import steammachinist.relexinternshiptask.dto.request.UpdateProductRequest;
+import steammachinist.relexinternshiptask.service.persistence.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping("/add")
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    public ResponseEntity<ProductDto> addProduct(@RequestBody AddProductRequest request) {
+        productService.add(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/update")
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    public ResponseEntity<String> updateProduct(@RequestBody UpdateProductRequest request) {
+        productService.update(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllResponses());
+    }
+}
