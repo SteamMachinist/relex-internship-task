9129a26728842fbea4941d45cc5dae9ed87c382a
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-03-18 14:57:30 +0300

    implemented service to handle register and login, returning jwt in result

---
+++ src/main/java/steammachinist/relexinternshiptask/service/authentication/AuthenticationService.java
@@ -0,0 +1,49 @@
+package steammachinist.relexinternshiptask.service.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import steammachinist.relexinternshiptask.dto.request.LoginRequest;
+import steammachinist.relexinternshiptask.dto.request.RegisterRequest;
+import steammachinist.relexinternshiptask.dto.response.JwtAuthenticationResponse;
+import steammachinist.relexinternshiptask.entity.User;
+import steammachinist.relexinternshiptask.service.persistence.UserService;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationResponse register(RegisterRequest request) {
+
+        User user = new User(
+                request.getName(),
+                request.getEmail(),
+                passwordEncoder.encode(request.getPassword()),
+                request.getRole());
+        userService.add(user);
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+    public JwtAuthenticationResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+        ));
+
+        UserDetails user = userService
+                .userDetailsService()
+                .loadUserByUsername(request.getEmail());
+
+        String jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+}
