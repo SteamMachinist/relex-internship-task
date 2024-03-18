c2e8314391688b2085b44b25caacbcdfdf27f3b8
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-03-18 18:57:34 +0300

    implemented ProductDto and requests

---
+++ src/main/java/steammachinist/relexinternshiptask/dto/ProductDto.java
@@ -0,0 +1,15 @@
+package steammachinist.relexinternshiptask.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String measurementUnit;
+    private boolean isInteger;
+}
